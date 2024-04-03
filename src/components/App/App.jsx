07b4baed9de688e11d02de0a2471eff9dd0e2a46
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-04-04 01:09:10 +0300

    Add folders jsx, css & title Cafe

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+// Imports components
+import Description from "../Description/Description";
+// import OptionsCafe from "../OptionsCafe/OptionsCafe";
+// import Feedback from "../Feedback/Feedback";
+
+export default function App() {
+  return (
+    <>
+      <Description desc="Sip Happens Café" />
+      {/* <OptionsCafe />
+      <Feedback /> */}
+    </>
+  );
+}
---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,11 @@
+export default function Description({ desc }) {
+  return (
+    <>
+      <h1>{desc}</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </>
+  );
+}
